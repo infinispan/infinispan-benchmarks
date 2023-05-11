11d07de66a83957dee63585d3e90a953523837f4
William Burns <mudokonman@gmail.com>
2023-05-11 14:18:42 -0700

    Update code for latest SNAPSHOT

--- resp-decoder/src/main/java/org/infinispan/server/resp/OurRespHandler.java
+++ resp-decoder/src/main/java/org/infinispan/server/resp/OurRespHandler.java
@@ -4,40 +4,21 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
-import java.util.function.IntFunction;
 
 import org.infinispan.commons.util.concurrent.CompletableFutures;
-import org.infinispan.util.function.TriConsumer;
 
-import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.util.AttributeKey;
 
 public class OurRespHandler extends RespRequestHandler {
    private static final CompletableFuture<byte[]> GET_FUTURE = CompletableFuture.completedFuture(new byte[] { 0x1, 0x12});
-   public static byte[] OK = "+OK\r\n".getBytes(StandardCharsets.US_ASCII);
-
    @Override
-   public CompletionStage<RespRequestHandler> actualHandleRequest(ChannelHandlerContext ctx, String type, List<byte[]> arguments) {
-      switch (type) {
-         case "GET":
-            return stageToReturn(GET_FUTURE, ctx, GET_TRICONSUMER);
-         case "SET":
-            return stageToReturn(CompletableFutures.completedNull(), ctx, OK_BICONSUMER);
+   protected CompletionStage<RespRequestHandler> actualHandleRequest(ChannelHandlerContext ctx, RespCommand command,
+                                                                     List<byte[]> arguments) {
+      if (command.getName().equals("GET")) {
+         return stageToReturn(GET_FUTURE, ctx, Consumers.GET_BICONSUMER);
+      } else if (command.getName().equals("SET")) {
+         return stageToReturn(CompletableFutures.completedNull(), ctx, Consumers.OK_BICONSUMER);
       }
-      return super.handleRequest(ctx, type, arguments);
+      return super.actualHandleRequest(ctx, command, arguments);
    }
-
-   protected static final BiConsumer<Object, ByteBufPool> OK_BICONSUMER = (ignore, alloc) ->
-         alloc.acquire(OK.length).writeBytes(OK);
-
-   protected static final BiConsumer<byte[], ByteBufPool> GET_TRICONSUMER = (innerValueBytes, alloc) -> {
-      if (innerValueBytes != null) {
-         bytesToResult(innerValueBytes, alloc);
-      } else {
-         stringToByteBuf("$-1\r\n", alloc);
-      }
-   };
 }
