81c7509efbf72481fcb0816d12ab607859fc0f52
William Burns <mudokonman@gmail.com>
2024-10-02 06:40:05 -0700

    Add support for running HR servers locally

--- getputremovetest/src/main/java/org/infinispan/InfinispanRemoteHolder.java
+++ getputremovetest/src/main/java/org/infinispan/InfinispanRemoteHolder.java
@@ -1,10 +1,16 @@
 package org.infinispan;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.LongAdder;
 
 import org.infinispan.client.hotrod.RemoteCache;
 import org.infinispan.client.hotrod.RemoteCacheManager;
 import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
+import org.infinispan.client.hotrod.impl.ConfigurationProperties;
+import org.infinispan.manager.DefaultCacheManager;
+import org.infinispan.server.hotrod.HotRodServer;
 import org.infinispan.server.hotrod.configuration.HotRodServerConfiguration;
 import org.infinispan.server.hotrod.configuration.HotRodServerConfigurationBuilder;
 import org.openjdk.jmh.annotations.Param;
@@ -16,42 +22,85 @@ import org.openjdk.jmh.annotations.TearDown;
 @State(Scope.Benchmark)
 public class InfinispanRemoteHolder {
 
-   @Param("localhost")
-   private String remoteHost;
+   private static final AtomicInteger hotRodPort = new AtomicInteger(ConfigurationProperties.DEFAULT_HOTROD_PORT);
 
-   @Param("11222")
-   private int remotePort;
+   static final String cfg = System.getProperty( "infinispan.cfg", "dist-sync.xml" );
 
-   @Param("1")
+   private final LongAdder gets = new LongAdder();
+   private final LongAdder puts = new LongAdder();
+   private final LongAdder removes = new LongAdder();
+
+   private final AtomicInteger cacheRequestCount = new AtomicInteger();
+
+   private DefaultCacheManager[] mgrs;
+   private RemoteCacheManager[] remotes;
+   private RemoteCache<?, ?>[] caches;
+
+   private HotRodServer[] servers;
+
+   @Param("3")
+   private int nodes;
+
+   @Param({"1", "3"})
+   private int remoteClients;
+
+   @Param({"-1"})
    private int maxPoolSize;
-
-   private RemoteCache cache;
-
-   private RemoteCacheManager remoteCacheManager;
 
    @Setup
    public void initializeState() throws IOException {
-      HotRodServerConfigurationBuilder configurationBuilder = new HotRodServerConfigurationBuilder();
-      configurationBuilder.port(remotePort).host(remoteHost);
-      HotRodServerConfiguration hotRodServerConfiguration = configurationBuilder.build();
-      System.out.printf("Started client %d\n", hotRodServerConfiguration.port());
+      mgrs = new DefaultCacheManager[nodes];
+      servers = new HotRodServer[nodes];
+      remotes = new RemoteCacheManager[remoteClients];
+      caches = new RemoteCache[remoteClients];
+      HotRodServerConfiguration firstConfig = null;
+
+      for (int i = 0; i < nodes; ++i) {
+         mgrs[i] = new DefaultCacheManager(cfg);
+         HotRodServerConfigurationBuilder configurationBuilder = new HotRodServerConfigurationBuilder();
+         configurationBuilder.port(hotRodPort.getAndIncrement());
+         HotRodServerConfiguration hotRodServerConfiguration = configurationBuilder.build();
+         if (firstConfig == null) {
+            firstConfig = hotRodServerConfiguration;
+         }
+         servers[i] = new HotRodServer();
+         servers[i].start(hotRodServerConfiguration, mgrs[i]);
+         System.out.printf("Started server %d\n", hotRodServerConfiguration.port());
+      }
 
       ConfigurationBuilder remoteCacheConfigurationBuilder = new ConfigurationBuilder();
-      remoteCacheConfigurationBuilder.disableTracingPropagation();
       remoteCacheConfigurationBuilder.connectionPool().maxActive(maxPoolSize);
-      remoteCacheConfigurationBuilder.addServer().host(hotRodServerConfiguration.host()).port(hotRodServerConfiguration.port());
-      remoteCacheManager = new RemoteCacheManager(remoteCacheConfigurationBuilder.build());
-
-      cache = remoteCacheManager.getCache("benchmark");
+      remoteCacheConfigurationBuilder.addServer().host(firstConfig.host()).port(firstConfig.port());
+      for (int i = 0; i < remoteClients; ++i) {
+         remotes[i] = new RemoteCacheManager(remoteCacheConfigurationBuilder.build());
+         caches[i] = remotes[i].getCache();
+      }
    }
 
    @TearDown
    public void shutdownState() {
-      remoteCacheManager.stop();
+      Arrays.stream(servers).forEach(HotRodServer::stop);
+      Arrays.stream(mgrs).forEach(DefaultCacheManager::stop);
+      System.out.println( "Gets performed: " + gets.longValue() );
+      System.out.println( "Puts performed: " + puts.longValue() );
+      System.out.println( "Removes performed: " + removes.longValue() );
    }
 
    public RemoteCache getCache() {
-      return cache;
+      int offset = cacheRequestCount.getAndIncrement();
+      return caches[offset % caches.length];
    }
 
+   public void cacheGetDone() {
+      gets.increment();
+   }
+
+   public void cachePutDone() {
+      puts.increment();
+   }
+
+   public void cacheRemoveDone() {
+      removes.increment();
+   }
 }
+
